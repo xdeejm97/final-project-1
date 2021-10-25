7eea86d643e9efc3856d4b6ca2b4096b106415db
xdeejm97 <d.niesmialowski@gmail.com>
2021-10-25 18:47:13 +0200

    first upload

---
+++ src/main/java/com/example/finalproject1/FinalProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.finalproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProject1Application.class, args);
+	}
+
+}
