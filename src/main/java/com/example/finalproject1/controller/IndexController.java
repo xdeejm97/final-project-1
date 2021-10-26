89c321e3d3dc9770166a052429bad7c597cadedc
xdeejm97 <d.niesmialowski@gmail.com>
2021-10-26 20:12:14 +0200

    htmls

--- src/main/java/com/example/finalproject1/controller/IndexController.java
+++ src/main/java/com/example/finalproject1/controller/IndexController.java
@@ -2,14 +2,17 @@ package com.example.finalproject1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHomePage(){
 
         return "index";
     }
+
 
 }
