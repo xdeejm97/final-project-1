6e9946995fbc8f7d08d94bd9ae395958b5d241f1
Teclis1 <teclis1@gmail.com>
2021-11-15 19:54:20 +0100

    registration commited

--- src/main/resources/static/js/register/main.js
+++ src/main/resources/static/js/register/main.js
@@ -1,80 +1,59 @@
-(function($) {
-
-  $('#meal_preference').parent().append('<ul class="list-item" id="newmeal_preference" name="meal_preference"></ul>');
-  $('#meal_preference option').each(function(){
-      $('#newmeal_preference').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
-  });
-  $('#meal_preference').remove();
-  $('#newmeal_preference').attr('id', 'meal_preference');
-  $('#meal_preference li').first().addClass('init');
-  $("#meal_preference").on("click", ".init", function() {
-      $(this).closest("#meal_preference").children('li:not(.init)').toggle();
-  });
-  
-  var allOptions = $("#meal_preference").children('li:not(.init)');
-  $("#meal_preference").on("click", "li:not(.init)", function() {
-      allOptions.removeClass('selected');
-      $(this).addClass('selected');
-      $("#meal_preference").children('.init').html($(this).html());
-      allOptions.toggle();
-  });
-
-  var marginSlider = document.getElementById('slider-margin');
-  if (marginSlider != undefined) {
-      noUiSlider.create(marginSlider, {
-            start: [500],
-            step: 10,
-            connect: [true, false],
-            tooltips: [true],
-            range: {
-                'min': 0,
-                'max': 1000
-            },
-            format: wNumb({
-                decimals: 0,
-                thousand: ',',
-                prefix: '$ ',
-            })
-    });
-  }
-  $('#reset').on('click', function(){
-      $('#register-form').reset();
-  });
-
-  $('#register-form').validate({
-    rules : {
-        first_name : {
-            required: true,
+$(function(){
+    $.validator.setDefaults({
+        highlight: function(element){
+            $(element)
+                .closest('.form-group')
+                .addClass('has-error')
         },
-        last_name : {
-            required: true,
-        },
-        company : {
-            required: true
-        },
-        email : {
-            required: true,
-            email : true
-        },
-        phone_number : {
-            required: true,
+        unhighlight: function(element){
+            $(element)
+                .closest('.form-group')
+                .removeClass('has-error')
         }
-    },
-    onfocusout: function(element) {
-        $(element).valid();
-    },
-});
-
-    jQuery.extend(jQuery.validator.messages, {
-        required: "",
-        remote: "",
-        email: "",
-        url: "",
-        date: "",
-        dateISO: "",
-        number: "",
-        digits: "",
-        creditcard: "",
-        equalTo: ""
     });
-})(jQuery);
+
+    $.validate({
+        rules:
+            {
+                password: "required",
+                birthDate: "required",
+                weight: {
+                    required:true,
+                    number:true
+                },
+                height:  {
+                    required:true,
+                    number:true
+                },
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+        messages:{
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        password: {
+            required: " Please enter password"
+        },
+        birthDate: {
+            required: " Please enter birthdate"
+        },
+        email: {
+            required: ' Please enter email',
+            email: ' Please enter valid email'
+        },
+        weight: {
+            required: " Please enter your weight",
+            number: " Only numbers allowed"
+        },
+        height: {
+            required: " Please enter your height",
+            number: " Only numbers allowed"
+        },
+    })
+
+});
