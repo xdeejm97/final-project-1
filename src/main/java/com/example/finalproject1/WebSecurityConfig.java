5d9141cee272525e59058d5ca20e981f61876dac
xdeejm97 <d.niesmialowski@gmail.com>
2021-11-22 18:25:16 +0100

    new changes

--- src/main/java/com/example/finalproject1/WebSecurityConfig.java
+++ src/main/java/com/example/finalproject1/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/css/**", "/js/**", "/register", "/plugins/**","/vendor/**","/images/**", "/", "/h2-console/**").permitAll()
+                .antMatchers("/css/**", "/js/**", "/register", "/plugins/**","/vendor/**","/images/**", "/", "/h2-console/**", "/terms-condition", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
